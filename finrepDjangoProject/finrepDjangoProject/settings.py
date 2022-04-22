7132fe4792bf373a36d6363d0b3d1d9d184a77fb
Angel Corrales <corralessoteloangel@outlook.com>
2022-04-21 18:34:48 -0600

    Cors configuración

--- finrepDjangoProject/finrepDjangoProject/settings.py
+++ finrepDjangoProject/finrepDjangoProject/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'backendEmployeeTest',
     'FinRepApp',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'finrepDjangoProject.urls'
 
