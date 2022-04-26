381530d5be2baf7ad2a009489e5f36c4bd616b01
Angel Corrales <corralessoteloangel@outlook.com>
2022-04-25 18:29:13 -0600

    gitignore

--- finrepDjangoProject/backendEmployeeTest/views.py
+++ finrepDjangoProject/backendEmployeeTest/views.py
@@ -1,3 +1,4 @@
+from sqlite3.dbapi2 import _AggregateProtocol
 from django.shortcuts import render, redirect
 from backendEmployeeTest.models import Employee
 from .serializer import EmployeeSerializer
@@ -7,4 +8,4 @@ from rest_framework import viewsets
 # Create your views here.
 class Employee(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+    serializer_class = EmployeeSerializer 
