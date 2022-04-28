ecd9db24447ff70da9a2bc64e90ddbf7650d3e8d
Angel Corrales <corralessoteloangel@outlook.com>
2022-04-28 12:38:20 -0600

    Json y tabla movimientos
    Test jason para balance
    Movimientos cambiado

--- finrepDjangoProject/FinRepApp/models.py
+++ finrepDjangoProject/FinRepApp/models.py
@@ -1,3 +1,4 @@
+from operator import truediv
 from django.db import models
 
 # Create your models here.
@@ -37,12 +38,14 @@ class Movimientos(models.Model):
     idEmpresa = models.ForeignKey('Empresa', on_delete=models.CASCADE)
     codigo = models.CharField(max_length=100)
     nombre = models.CharField(max_length=100)
+    concepto = models.CharField(max_length=100, null=True)
+    referencia = models.CharField(max_length=100, null=True)
     saldoInicial = models.FloatField()
-    totalCargos = models.FloatField()
-    totalAbonos = models.FloatField()
-    saldoAcumulado = models.FloatField()
-    fechaInicio = models.DateField()
-    fechaFinal = models.DateField()
+    totalCargos = models.FloatField(null=True)
+    totalAbonos = models.FloatField(null=True)
+    # saldoAcumulado = models.FloatField()
+    fecha = models.DateField(null=True)
+
 
     class Meta:
         db_table = "movimientos"
