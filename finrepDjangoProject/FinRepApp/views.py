018fef615c68ea0068b889b73db6b4ca3be9d1fb
Angel Corrales <corralessoteloangel@outlook.com>
2022-05-18 12:33:52 -0600

    Mandar empresas y recibir 2 archivos

--- finrepDjangoProject/FinRepApp/views.py
+++ finrepDjangoProject/FinRepApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from rest_framework.decorators import APIView
 from FinRepApp.models import Cuentas
-from .serializer import CuentasSerializer
+from .serializer import *
 from rest_framework import viewsets
 from FinRepApp.Utils.balanceGeneralFormatting import *
 from FinRepApp.Utils.fixingMovimientosDFToInsert import *
@@ -29,6 +29,41 @@ def prueba(request):
     print(json_object)
     return Response(balance)
 
+class Usuario_EmpresaViewSet(viewsets.ViewSet):
+    queryset = Usuario_Empresa.objects.all()
+
+    def retrieve(self, request, pk):
+        for e in Usuario_Empresa.objects.all():
+            print(e)
+        print(pk, "esto es el pk")
+        # queryset = Usuario_Empresa.objects.filter(idUsuario_id=pk).only('idEmpresa')
+        queryset = Usuario_Empresa.objects.filter(idUsuario_id=pk)
+        print(queryset.count(), "cuenta de queryset")
+        # no usar serializer tal vez, puedo únicamente convertir los nombres directamente en un JSON
+        # devolver solo el nombre no sé si esté bien porque hay que tener el id tal vez más bien o guardarlo en alguna variable
+        # o algo así para poder usarlo luego en los reportes y la subida de archivo
+        # {
+        # empresas:{"Lecar":1, "Walmart":3, "Ereh":3} , 
+        # informaciónStatus: [] 
+        # }
+        json = {'empresas':{} , 'status': []}
+        # json = {'empresas':{} , 'informacionStatus': []}
+        for e in queryset:
+            json['empresas'][e.idEmpresa.nombre] = e.idEmpresa.id
+            # json['informacionStatus'][e.idEmpresa.nombre] = e.idUsuario.id
+            # print(e.idUsuario.id)
+
+        
+        # print(queryset.attribute)
+        # serializer = Usuario_EmpresaSerializer(queryset, many=True)
+        # serializer = Usuario_EmpresaSerializer(queryset)
+        return Response(json)
+        print("jalo?")
+        if serializer.is_valid():
+            print(serializer.data)
+        else:
+            return Response(serializer.errors)
+
 @api_view(['GET'])
 def getMovimientos(request):
     init_db()
@@ -45,6 +80,29 @@ def getMovimientos(request):
     #print(json, "JSON")
     # return Response({"Valor de linea 0 columna 0":df_listo.iat[0,0]})
     return Response(json_object)
+
+@api_view(['GET'])
+def getMovimientosTest(request,idEmpresa):
+    print(idEmpresa, "esto es el idEmpresa")
+    init_db()
+    dictionary ={ 
+    "id": "04", 
+    "name": "sunil", 
+    "department": "HR"
+    } 
+    balanceGeneral = acomodarResponse(movimientoshacker())
+    # Serializing json  
+    json_object = js.dumps(balanceGeneral) 
+    print(json_object)
+
+    #print(json, "JSON")
+    # return Response({"Valor de linea 0 columna 0":df_listo.iat[0,0]})
+    return Response(json_object)
+
+@api_view(['GET'])
+def getEmpresas(request,idUsuario):
+    pass
+
 
 @api_view(['POST'])
 def uploadMovimientos(request):
