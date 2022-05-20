5c496bb84fbe756480931c94cb155b9e80da8892
Santiago Arzaga <73621292+SantiagoArzaga@users.noreply.github.com>
2022-05-20 00:50:26 -0600

    Funcionalidad de login y registro
    Funcionalida del backend terminada
    
    Co-Authored-By: Isaac Garcia <70421623+OnsideStg@users.noreply.github.com>

--- finrepDjangoProject/FinRepApp/Utils/login.py
+++ finrepDjangoProject/FinRepApp/Utils/login.py
@@ -1,30 +1,35 @@
+from tokenize import String
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
-
+import json
 
 #user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
 
 #Registro del usuario
 def register(request):
     if request.method == 'POST':
+        datos = json.loads(request.body)
         print("saludos")
-        username = request.POST.get('username')
-        email = request.POST.get('username')
-        password = request.POST.get('username')
+        username = datos["username"]
+        email = datos["email"]
+        password = datos["password"]
         user = User.objects.create_user(username, email, password)
         return HttpResponse()
 
 #Login del usuario
 def my_view(request):
-    username = request.POST.get('username')
-    password = request.POST.get('password')
+    datos = json.loads(request.body)
+    username = datos["username"]
+    password = datos["password"]
     user = authenticate(username=username, password=password)
     if user is not None:
         login(request, user)
         print("login exitoso")
+        print(user.id)
+        #print(password)
         return HttpResponse()
-        #print(user.id)
+        
         # Redirect to a success page.
         #Return exito
         ...
